c5128598ae95a497e1eea08fdf0b521c81ee7f6d
Wang <0656152@students.dadeschools.net>
2019-02-22 08:04:33 -0500

    v3

--- main.js
+++ main.js
@@ -11,29 +11,36 @@ let car = {
         this.mileage += 10;
         alert(`New Mileage: ${this.mileage}`);
     },
-}
+    driveAroundTW(){
+        alert(`Old Mileage: ${this.mileage}`);
+        this.mileage += 7000;
+        alert(`New Mileage: ${this.mileage}`);
+        alert('Car needs a tuneup!');
+        this.isWorking = false;
+    },
+    
+    getTuneUp(){
+        alert('Car is fixed and ready to go!');
+        this.isWorking = true;
+    },
+b
+    honk(){
+        alert("Honk! Honk!");
+    }
+};
 
 // FUNCTIONS
 // Logs all of our car's current stats to the console
-const reWriteStats = () => {
-    console.log(`Make: $(car.make)`);
-    console.log(`Model: $(car.model)`);
-    console.log(`Color: $(car.color)`);
-    console.log(`Mileage: $(car.mileage)`);
-    console.log(`Is Working: $(car.isWorking)`);
-    console.log(`(................................)`);
+reWriteStats = () => {
+    console.log(`Make: ${car.make}`);
+    console.log(`Model: ${car.model}`);
+    console.log(`Color: ${car.color}`);
+    console.log(`Mileage: ${car.mileage}`);
+    console.log(`IsWorking: ${car.isWorking}`);
+    console.log('-----------------------------');
 }
 
 
-
-
-
-
-
-function reWriteStats(){
-
-}
-*/
 
 // MAIN PROCESS
 // Captures keyboard input. Depending on the letter pressed it will "call" (execute) different functions.
@@ -43,6 +50,21 @@ document.onkeyup = (event) => {
 
     if (letter === "d") {
         car.driveToWork();
+        reWriteStats();
+    }
+
+    if (letter === "r") {
+        car.driveAroundUSA();
+        reWriteStats();
+    }
+
+    if (letter === "t") {
+        car.getTuneUp();
+        reWriteStats();
+    }
+
+    if (letter === "h") {
+        car.honk();
         reWriteStats();
     }
 
